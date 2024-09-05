d7d8e6e5831554cd809cb25b3b2d8f8416a1b593
rluispdev <rluisp@icloud.com>
2024-09-05 15:37:47 -0300

    Adicionar um Loop

--- app.js
+++ app.js
@@ -3,13 +3,16 @@
 
 let section = document.getElementById("resultados-pesquisa")
 
-//Injetando HTML a partir do JavaScript
-section.innerHTML = `
-<div class="item-resultado">
-            <h2> 
-                <a href="#" target="_blank"> ${jogadores[0].titulo} </a> 
-                 </h2>
-            <p class="descricao-meta"> Neymar da Silva Santos Júnior, mais conhecido como Neymar Jr., é um dos maiores craques do futebol brasileiro da atualidade. Reconhecido mundialmente por seu talento, dribles desconcertantes e habilidade de marcar gols, Neymar já vestiu as camisas de grandes clubes como Santos, Barcelona e Paris Saint-Germain. </p>
-            <a href="https://pt.wikipedia.org/wiki/Neymar" target= "_blank"> Saiba Mais... </a>
-        </div>
-`
+for(let jogador of jogadores){
+    section.innerHTML = `
+    <div class="item-resultado">
+                <h2> 
+                    <a href="#" target="_blank"> ${jogador.titulo} </a> 
+                     </h2>
+                <p class="descricao-meta"> ${jogador.descricao} </p>
+                <a href= ${jogador.link} target= "_blank"> Saiba Mais... </a>
+            </div>
+    `
+}
+ 
+
