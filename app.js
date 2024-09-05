e33ef7af98177671d0c9d28989e717ae8edac7af
rluispdev <rluisp@icloud.com>
2024-09-05 15:27:09 -0300

    Injetando HTML a partir do JavaScrip

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 
-console.log(jogadores);
+ 
 
 let section = document.getElementById("resultados-pesquisa")
+
+//Injetando HTML a partir do JavaScript
+section.innerHTML = `
+<div class="item-resultado">
+            <h2> 
+                <a href="#" target="_blank">Neymar Junior </a> 
+                 </h2>
+            <p class="descricao-meta"> Neymar da Silva Santos Júnior, mais conhecido como Neymar Jr., é um dos maiores craques do futebol brasileiro da atualidade. Reconhecido mundialmente por seu talento, dribles desconcertantes e habilidade de marcar gols, Neymar já vestiu as camisas de grandes clubes como Santos, Barcelona e Paris Saint-Germain. </p>
+            <a href="https://pt.wikipedia.org/wiki/Neymar" target= "_blank"> Saiba Mais... </a>
+        </div>
+`
