c228c6f2e217024382ee7109ae6f1250e344b5f5
rluispdev <rluisp@icloud.com>
2024-09-06 10:27:57 -0300

    Criar condicionais

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ function pesquisar() {
 
     let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-    console.log(campoPesquisa)
-  
+  if (campoPesquisa == "") {
+    section.innerHTML = "Digite um nome de jogador."
+    return
+  }
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
   
     // Itera sobre cada jogador no array 'jogadores'
     for (let jogador of jogadores) {
-      // Constrói o HTML para cada jogador, utilizando template literals para inserir os dados dinamicamente
-      resultados += `
+    
+
+      if (jogador.titulo.includes (campoPesquisa)) {
+        // Constrói o HTML para cada jogador, utilizando template literals para inserir os dados dinamicamente
+        resultados += `
         <div class="item-resultado">
           <h2>
             <a href="#" target="_blank">${jogador.titulo}</a>
@@ -21,8 +26,9 @@ function pesquisar() {
           <a href="${jogador.link}" target="_blank">Saiba Mais...</a>
         </div>
       `;
+      }
     }
-  
+
     // Atribui o HTML gerado para a propriedade innerHTML da seção, substituindo o conteúdo anterior
     section.innerHTML = resultados;
   }
