4ec7938b2f8721930875d34b7228b19fac66fd40
rluispdev <rluisp@icloud.com>
2024-09-06 10:43:42 -0300

    Adicionar tags aos dados

--- dados.js
+++ dados.js
@@ -4,34 +4,39 @@ let jogadores = [
       descricao: "Neymar da Silva Santos Júnior, mais conhecido como Neymar Jr., é um dos maiores craques do futebol brasileiro da atualidade. Reconhecido mundialmente por seu talento, dribles desconcertantes e habilidade de marcar gols, Neymar já vestiu as camisas de grandes clubes como Santos, Barcelona e Paris Saint-Germain",
       link: "https://pt.wikipedia.org/wiki/Neymar",
       idade: 31, 
-      timeAtual: "Al-Hilal Saudi Football Club" 
+      timeAtual: "Al-Hilal Saudi Football Club" ,
+      tags: " menino lupo puma"
     },
     {
       titulo: "Kylian Mbappé",
       descricao: "Kylian Mbappé Lottin é um futebolista francês que atua como atacante. Considerado um dos melhores jogadores do mundo, Mbappé se destacou no Monaco antes de se transferir para o Paris Saint-Germain.",
       link: "https://pt.wikipedia.org/wiki/Kylian_Mbappé",
       idade: 24,
-      timeAtual: "Real Madrid"
+      timeAtual: "Real Madrid",
+      tags: "frança  atacante"
     },
     {
       titulo: "Pedro",
       descricao: "Pedro Guilherme Abreu dos Santos, mais conhecido como Pedro, é um futebolista brasileiro que atua como atacante. Atualmente, defende as cores do Flamengo.",
       link: "https://pt.wikipedia.org/wiki.org/Pedro_Guilherme_Abreu_dos_Santos",
       idade: 26,
-      timeAtual: "Flamengo"
+      timeAtual: "Flamengo",
+      tags: "9 nove atacante"
     },
     {
       titulo: "De Arrascaeta",
       descricao: "Giorgian De Arrascaeta Reyes é um futebolista uruguaio que atua como meia. Atualmente, defende as cores do Flamengo.",
       link: "https://pt.wikipedia.org/wiki/Giorgian_De_Arrascaeta",
       idade: 29,
-      timeAtual: "Flamengo"
+      timeAtual: "Flamengo",
+      tags: "uruguai celeste meia"
     },
     {
       titulo: "Gabigol",
       descricao: "Gabriel Barbosa Almeida, mais conhecido como Gabigol, é um futebolista brasileiro que atua como atacante. Atualmente, defende as cores do Flamengo.",
       link: "https://pt.wikipedia.org/wiki/Gabriel_Barbosa_Almeida",
       idade: 27,
-      timeAtual: "Flamengo"
+      timeAtual: "Flamengo",
+      tags: " atacante forte"
     }
   ];
